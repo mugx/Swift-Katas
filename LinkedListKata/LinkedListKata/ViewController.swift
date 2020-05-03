2405e1903ae6ce833a6d948bd5b777d92f026b6d
mugx <andrea.mugx@gmail.com>
2020-05-03 19:55:49 +0100

    Adds linked list kata implementation

---
+++ LinkedListKata/LinkedListKata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LinkedListKata
+//
+//  Created by mugx on 03/05/2020.
+//  Copyright © 2020 mugx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
